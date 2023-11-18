bae4b21738c72666d3fa347f6d7b0a60144f89e7
mathiasaertsens <math.aertsens@gmail.com>
2023-11-18 23:53:24 +0100

    integration tailwind login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -16,7 +16,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-<div class="min-h-screen flex flex-col pt-6 sm:pt-0 bg-gray-100 dark">
+<div class="min-h-screen flex flex-col pt-6 sm:pt-0 bg-gray-100 ">
     <div class="container mx-auto flex flex-col space-y-10">
         <nav class="flex justify-between items-center py-2">
             <div>
@@ -24,7 +24,7 @@
                    class="group font-bold text-3xl flex items-center space-x-4 hover:text-emerald-600 transition ">
                     <x-application-logo
                         class="w-10 h-10 fill-current text-gray-500 group-hover:text-emerald-500 transition" />
-                    <span>Instamath</span>
+                    <span>InstaMath</span>
                 </a>
             </div>
             <div class="flex items-center space-x-4">
@@ -33,10 +33,11 @@
                 <a href="{{ route('register') }}"
                    class="font-semibold text-gray-700 hover:text-emerald-600 transition">Register</a>
             </div>
+
         </nav>
 
         <main>
-            <div class=" ">
+            <div class="grid grid-cols-1 gap-8">
                 {{ $slot }}
             </div>
         </main>

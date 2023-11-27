f908f565e4122d0f8d9ac383ee33ae04b49a5315
mathiasaertsens <math.aertsens@gmail.com>
2023-11-27 11:55:14 +0100

    affichage des commentaires

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -15,41 +15,30 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="font-sans text-gray-900 antialiased">
-<div class="min-h-screen flex flex-col pt-6 sm:pt-0 bg-gray-100 ">
-    <div class="container mx-auto flex flex-col space-y-10">
-        <nav class="flex justify-between items-center py-4">
-            <div>
-                <a href="/"
-                   class="group font-bold text-4xl flex items-center space-x-4 hover:text-emerald-600 transition ">
-                    <x-application-logo
-                        class="w-10 h-10 fill-current text-gray-500 group-hover:text-emerald-500 transition" />
-                    <span class="font-serif">InstaMath</span>
-                </a>
+<body class="font-sans text-gray-900 antialiased bg-gray-100 min-h-screen flex items-center justify-center">
+<div class="container mx-auto">
+    <div class="flex flex-col items-center space-y-10">
+        <nav class="flex justify-between items-center py-4 w-full">
+            <a href="/" class="group font-bold text-4xl flex items-center space-x-4 hover:text-emerald-600 transition">
+                <x-application-logo class="w-10 h-10 fill-current text-gray-500 group-hover:text-emerald-500 transition" />
+                <span class="font-serif">InstaMath</span>
+            </a>
 
-
-            </div>
             <div class="flex items-center space-x-4">
                 <a href="{{ route('profile.update') }}" class="flex items-center font-medium text-gray-600 hover:text-gray-500 transition">
                     <img src="{{ asset('storage/' . Auth::user()->profile_photo) }}" alt="Photo de profil" class="w-10 h-10 rounded-full">
                     {{ Auth::user()->name }}
                 </a>
-            </div>
-            <div>
 
                 <form action="{{ route('logout') }}" method="post">
                     @csrf
-                    <button type="submit"
-                            class="font-medium text-gray-600 hover:text-gray-500 transition">Déconnexion
-                    </button>
+                    <button type="submit" class="font-medium text-gray-600 hover:text-gray-500 transition">Déconnexion</button>
                 </form>
-
             </div>
-
         </nav>
 
-        <main>
-            <div class="grid grid-cols-1 gap-8">
+        <main class="flex justify-center items-center w-full">
+            <div class="grid grid-cols-1 gap-8 max-w-screen-md w-full">
                 {{ $slot }}
             </div>
         </main>
--- resources/views/pages/feed.blade.php
+++ resources/views/pages/feed.blade.php
@@ -1,16 +1,31 @@
 <x-user-layout>
     <h1>Liste des posts</h1>
-    <ul class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+    <ul class="grid sm:grid-cols-1 lg:grid-cols-1 2xl:grid-cols-1 justify-center">
         @foreach ($posts as $post)
-            <li>
+            <li style="max-width: 400px; margin: 0 auto;">
                 <p>{{ $post->user->name }}</p>
-                <img src="{{ asset($post->user->profile_photo_url) }}" alt="{{ $post->user->name }}'s profile photo">
-                <p><a href="{{ route('posts.show', ['id' => $post->id]) }}">Voir les détails</a></p>
+                <img src="{{ asset($post->user->profile_photo) }}" alt="{{ $post->user->name }}'s profile photo" class="w-10 h-10 rounded-full">
+                <a href="{{ route('posts.show', ['id' => $post->id]) }}">Voir les détails</a>
                 <img src="{{ asset($post->image_url) }}" alt="{{ $post->description }}">
                 <p>{{ $post->description }}</p>
 
+                {{-- Afficher les commentaires --}}
+                <ul class="bg-gray-300 p-4 rounded-md">
+                    <h3>Commentaires :</h3>
+                @foreach ($post->commentaires as $commentaire)
+                        <li>
+                            <img src="{{ asset($commentaire->user->profile_photo) }}" alt="{{ $commentaire->user->name }}'s profile photo" class="w-6 h-6 rounded-full">
+                            <p>{{ $commentaire->user->name }} :</p>
+                            <p>{{ $commentaire->content }}</p>
+                            <p>Date : {{ $commentaire->date }}</p>
+                        </li>
+                    @endforeach
+                </ul>
+
+            {{-- Ajouter un commentaire --}}
 
             </li>
         @endforeach
     </ul>
 </x-user-layout>
+
